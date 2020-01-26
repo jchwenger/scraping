e95b28c70a51569ad3a90ae838c1055516a2c974
jchwenger <34098722+jchwenger@users.noreply.github.com>
2020-01-26 16:00:11 +0000

    Download work by one author with option to specify language | improved clean-up script

---
+++ gutenberg/clean.py
@@ -0,0 +1,136 @@
+import os
+import re
+import argparse
+from shutil import copyfile
+
+
+def main(args):
+    input_dir = args.dir
+    output_dir = input_dir + "-clean"
+    make_dir(output_dir)
+    files = os.listdir(input_dir)
+    transfer(files, input_dir, output_dir)
+
+
+def transfer(files, input_dir, output_dir):
+    total = len(files)
+    for i, fname in enumerate(files):
+        msg = f"file {i+1:4}/{total}, name: {fname}"
+        blank = os.get_terminal_size()[0] - len(msg)
+        print(msg + " " * blank)
+        f = openfile(os.path.join(input_dir, fname))
+        eoh = end_of_header(f, fname)
+        sof = start_of_footer(f, fname)
+        stripped_f = strip_lines(f[eoh:sof])
+        writefile(os.path.join(output_dir, fname), stripped_f)
+
+
+def end_of_header(lines, fname):
+    start = 0
+    # try to find *** START OF TH... or end
+    pattern = re.compile(
+        r"(^ *\*+ *start of th|^ *\*+.*end.*the small print)", re.IGNORECASE
+    )
+    limit = len(lines) // 3  # only search the first third of the document
+    for i, l in enumerate(lines[:limit]):
+        if re.match(pattern, l):
+            ind = 0  # find the end of the paragraph
+            while not "\n" == lines[i + ind]:
+                ind += 1
+            start = i + ind
+            break
+    if start == 0:
+        print(f"Header not found: {fname}")
+    # try to find lines starting with 'Produced/Distributed'
+    pattern = re.compile(r"(.*etext.*prepared|^produced|^distributed)", re.IGNORECASE)
+    for i, l in enumerate(lines[start : start + 50]):
+        if re.match(pattern, l):
+            ind = 1
+            # find the end of the paragraph
+            while not "\n" == lines[start + i + ind]:
+                ind += 1
+            # remove empty lines
+            start = start + i + ind
+            break
+    # try to find a title
+    # for i, l in enumerate(lines[start : start + 50]):
+    #     if l.isupper():
+    #         start = start + i
+    #         break
+    return start
+
+
+def start_of_footer(lines, fname):
+    pattern = re.compile(r"^(\*\*\*)* *end of.*project gutenberg", re.IGNORECASE)
+    start = 0
+    for i, l in enumerate(lines):
+        if re.match(pattern, l):
+            start = i - 1
+            break
+    if start == 0:
+        print(f"Footer not found: {fname}, start: {start}")
+        start = len(lines) - 1
+    return start
+
+
+def strip_lines(lines):
+    start = 0
+    end = len(lines) - 1
+    for i, l in enumerate(lines):
+        if "\n" != l:
+            start = i
+            break
+    for i, l in enumerate(reversed(lines)):
+        if "\n" != l:
+            end = len(lines) - i
+            break
+    return lines[start:end]
+
+
+def reencode(lines, fname):
+    newlines = []
+    if "ï»¿" in lines[0]:
+        for i in range(len(lines)):
+            try:
+                newlines.append(lines[i].encode("latin-1").decode("utf-8"))
+            except:
+                newlines.append(lines[i].encode("latin-1").decode("utf-8", "ignore"))
+                print(f"ignoring error in file {fname}, line: {i}:")
+                print(f"before: {lines[i]}")
+                print(f"after: {newlines[-1]}")
+        return newlines
+    return lines
+
+
+# utils
+
+
+def openfile(fname):
+    try:
+        with open(fname, "r", encoding="utf-8") as f:
+            lines = f.readlines()
+    except:
+        with open(fname, "r", encoding="latin-1") as f:
+            lines = f.readlines()
+    lines = reencode(lines, fname)
+    return lines
+
+
+def writefile(fname, lines):
+    with open(fname, "w") as wr:
+        wr.writelines(lines)
+
+
+def make_dir(output_dir):
+    if os.path.isdir(output_dir):
+        return
+    os.mkdir(output_dir)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Copying & cleaning Gutenberg files")
+    parser.add_argument(
+        "-d", "--dir", type=str, help="The input directory with raw files"
+    )
+    args = parser.parse_args()
+    main(args)
---
+++ gutenberg/gutenberg-author.py
@@ -0,0 +1,122 @@
+import os
+import re
+import re
+import json
+import random
+import pprint
+import urllib3
+import certifi
+import argparse
+from time import sleep
+from bs4 import BeautifulSoup
+
+
+def main(args):
+    author = args.author
+    auth_re = re.compile('^'+author, re.IGNORECASE) 
+    lang = re.compile(args.language, re.IGNORECASE)
+    delay = args.delay
+    # browse page with first letter
+    letter = author[0].lower()
+    url = 'http://www.gutenberg.org/browse/authors/' + letter
+    # request, get & soup
+    http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
+    r = http.request('GET', url)
+    soup = BeautifulSoup(r.data, 'html.parser')
+    # get links to all ebooks
+    links = get_links(auth_re, soup, lang)
+    # make result dir
+    author_dir = make_dir(author)
+    total = len(list(links.keys()))
+    print(f'{total} links in total')
+    i = 1
+    for title, link in links.items():
+        save_ebook(title, link, author_dir, i, total, delay, http)
+        i += 1
+
+def get_links(auth_re, soup, lang):
+    # find section
+    h2 = soup.find_all(string=auth_re)
+    choice = 0
+    if len(h2) > 1:
+        print('more than one possibility, please choose:')
+        for i, n in enumerate(h2):
+            print(f'{i}: {n}')
+        choice = int(input())
+    # find list of books
+    ul = h2[choice].next
+    while ul.name != 'ul':
+        ul = ul.next
+    # only direct authorship
+    as_auth = re.compile('as Author')
+    links = {}
+    for li in ul.find_all(class_='pgdbetext'):
+        if li.find(string=as_auth) and li.find(string=lang):
+            links[li.a.text.replace('\r',' ')] = li.a['href']
+    return links
+
+def save_ebook(title, link, author_dir, i, total, delay, http):
+    # format title
+    riddance = re.compile('[!?.()"";:,\']')
+    t = re.sub(riddance, '', title.lower()).replace(' ', '-') + '.txt'
+    if not os.path.isfile(os.path.join(author_dir, t)):
+        ebook = get_ebook(link, http, secs=delay)
+        l = len(str(total))
+        print(f'{i:{l}}/{total}, saving: {t}')
+        with open(os.path.join(author_dir, t), 'wb') as f:
+            f.write(ebook)
+    else:
+        print(f'{i:4}/{total}, already downloaded {t}, continuing')    
+
+def get_ebook(link, http, secs=1):
+    G = 'http://www.gutenberg.org'
+    # go to list of formats
+#     http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
+    r = http.request('GET', G+link)
+    s = BeautifulSoup(r.data, 'html.parser')
+    # find utf-8 plain text version
+    txt = re.compile('plain text', re.IGNORECASE)
+    l = s(string=txt)[0].parent['href'] # link
+    sleep(secs) # get thee not blocked
+    # download ebook & return it
+    h = http.request('Get', G+l)
+    return(h.data)
+
+def make_dir(author):
+    author_dir = '-'.join(map(str.lower, author.split(', ')))
+    if not os.path.isdir(author_dir):
+        print(f'creating new directory {author_dir}')
+        os.mkdir(author_dir)
+    else: print(f'{author_dir} already exists')
+    return author_dir
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Downloading all books by one author from Gutenberg"
+    )
+    parser.add_argument(
+        "-a",
+        "--author",
+        type=str,
+        default='Shakespeare, William',
+        help="""The author's name in such a format: 'lastname, first name'. 
+                Case insensitiv.e Must start by the same character the same as
+                on the gutenberg browse page, e.g.  
+                http://www.gutenberg.org/browse/authors/a""",
+    )
+    parser.add_argument(
+        "-l",
+        "--language",
+        type=str,
+        default="English",
+        help="The language of the selected ebooks, default: any",
+    )
+    parser.add_argument(
+        "-d",
+        "--delay",
+        type=float,
+        default=1,
+        help="The delay to wait between each download, default: 1 second",
+    )
+    args = parser.parse_args()
+    main(args)
